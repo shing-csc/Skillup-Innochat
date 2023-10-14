a7b36ad02d9b692e3fe17fd19cff6c8034b27058
daniel chow <128502218+shing-csc@users.noreply.github.com>
2023-10-14 15:50:02 +0800

    Update index.tsx
    - Converted the hash router to browser router
    - Removed the routes and add it inside the <Main/>

--- app/frontend/src/index.tsx
+++ app/frontend/src/index.tsx
@@ -1,25 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 import { initializeIcons } from "@fluentui/react";
 
 import "./index.css";
 
-import Chat from "./pages/chat/Chat";
+import Main from "./pages/Main"
+
 
 initializeIcons();
 
 export default function App() {
     return (
-        // <React.StrictMode>
-        //     <Layout />
-        //     <Chat />
-        // </React.StrictMode>
-        <HashRouter>
-            <Routes>
-                <Route path="/" element={<Chat />}/>
-            </Routes>
-        </HashRouter>
+        <BrowserRouter>
+            <Main/>
+        </BrowserRouter>
     );
 }
 
